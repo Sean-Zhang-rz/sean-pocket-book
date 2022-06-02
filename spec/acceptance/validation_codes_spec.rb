46af3d3f0b2e3a98049e8b48d35be3abaadd3462
Sean <770899447@qq.com>
2022-06-02 10:55:55 +0800

    引入库解决响应体不是叫上你

--- spec/acceptance/validation_codes_spec.rb
+++ spec/acceptance/validation_codes_spec.rb
@@ -4,13 +4,14 @@ require 'rspec_api_documentation/dsl'
 resource "验证码" do
   post "/api/v1/validation_codes" do
     parameter :email, type: :string, required: true, desc: "邮箱"
-    header "Content-Type", "application/json"
     let(:email) { ['1@qq.com'] }
 
     example "请求发送验证码" do
       do_request
 
       expect(status).to eq 200
+      # json = JSON.parse(response_body)
+      expect(response_body).to eq ' '
     end
   end
 end
