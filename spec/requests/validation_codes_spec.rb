74148e1e1e5dbaa47c3fa193d2df53170355f11a
Sean <770899447@qq.com>
2022-06-17 08:20:14 +0800

    删除多余文件

--- spec/requests/validation_codes_spec.rb
+++ spec/requests/validation_codes_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "ValidationCodes", type: :request do
   describe "发送验证码" do
-    it "验证码可以被发送" do
+    it "发送太频繁了" do
       post '/api/v1/validation_codes', params: {email: '770899447@qq.com'}
       expect(response).to have_http_status(200)
+      post '/api/v1/validation_codes', params: {email: '770899447@qq.com'}
+      expect(response).to have_http_status(429)
     end
   end
 end
