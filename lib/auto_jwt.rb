f1921dfb73ea9070ace330fa697fed5050e1b4d1
Sean <770899447@qq.com>
2022-10-24 22:38:51 +0800

    测试jwt过期

--- lib/auto_jwt.rb
+++ lib/auto_jwt.rb
@@ -4,9 +4,18 @@ class AutoJwt
   end
 
   def call(env)
+    # jwt跳过以下检查
+    return @app.call(env) if ['/api/v1/session'].include? env['PATH_INFO']
+
     header = env['HTTP_AUTHORIZATION']
     jwt = header.split(' ')[1] rescue ' '
-    payload = JWT.decode jwt, Rails.application.credentials.hmac_secret, true, { algorithm: 'HS256' } rescue nil
+    begin
+      payload = JWT.decode jwt, Rails.application.credentials.hmac_secret, true, { algorithm: 'HS256' } 
+    rescue JWT::ExpiredSignature
+      return [401, {}, [JSON.generate({reason: 'token已过期'})]]
+    rescue
+      return [401, {}, [JSON.generate({reason: 'token无效'})]]
+    end
     env['current_user_id'] = payload[0]['user_id'] rescue nil 
     # 状态码，响应头，响应体 执行所有controller 
     @status, @headers, @response = @app.call(env)
--- spec/requests/api/v1/mes_spec.rb
+++ spec/requests/api/v1/mes_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
+require 'active_support/testing/time_helpers'
 
 RSpec.describe "Me", type: :request do
+  include ActiveSupport::Testing::TimeHelpers
   describe "获取当前用户" do
     it "登录后成功获取" do
       user = User.create email: '770899447@qq.com'
@@ -12,6 +14,24 @@ RSpec.describe "Me", type: :request do
       expect(response).to have_http_status(200)
       json = JSON.parse response.body
       expect(json['resource']['id']).to eq user.id
+    end
+    it "jwt 过期" do
+      travel_to Time.now - 3.hours
+      user1 = User.create email: '1@qq.com'
+      jwt = user1.generate_jwt
+
+      travel_back
+      get '/api/v1/me', headers: {'Authorization': "Bearer #{jwt}"}
+      expect(response).to have_http_status(401)
+    end
+    it "jwt 没过期" do
+      travel_to Time.now - 1.hours
+      user1 = User.create email: '1@qq.com'
+      jwt = user1.generate_jwt
+
+      travel_back
+      get '/api/v1/me', headers: {'Authorization': "Bearer #{jwt}"}
+      expect(response).to have_http_status(200)
     end
   end
 end 
